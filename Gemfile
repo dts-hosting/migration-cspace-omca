87f68989918c6aee0850b12a2c8d7a54526fdee8
Kristina Spurgin <kspurgin@users.noreply.github.com>
2026-04-28 13:46:28 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem "kiba-extend", github: "lyrasis/kiba-extend", branch: "main"
+
+group :development do
+  gem "almost_standard", github: "kspurgin/almost_standard", branch: "main"
+  gem "pry"
+end
+
+group :test do
+  gem "rspec"
+  gem "rspec-custom",
+    github: "kspurgin/rspec-custom",
+    branch: "main"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "bundler/setup"
+
+# pulls in kiba-extend's helpers.rb, which lets you use existing methods for
+#   setting up and running transform tests
+require "kiba/extend"
+kiba_spec_dir = "#{Gem.loaded_specs["kiba-extend"].full_gem_path}/spec"
+Dir.glob("#{kiba_spec_dir}/*").sort.select do |path|
+  path.match?(/helpers\.rb$/)
+end.each do |rbfile|
+  require rbfile
+end
+
+require_relative "../lib/ke_project"
+
+# A custom rspec matcher to compare expected and given CSVs and provide a usable
+#   diff. Use this if you are testing the output of a job. See example of use in
+#   https://github.com/lyrasis/kiba-tms/blob/main/spec/kiba/tms/mixins/reportable_for_table_spec.rb
+require "rspec/custom/matchers/match_csv"
+
+RSpec.configure do |config|
+  config.extend KeProject
+  config.include Helpers
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
