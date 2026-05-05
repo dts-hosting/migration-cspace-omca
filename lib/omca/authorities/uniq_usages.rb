1e307f8f85b59eac465f4cc75e35aa0c9c3384ae
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-05-05 19:14:59 -0400

    refactor: Extract thor at uniq_usages to class

---
+++ lib/omca/authorities/uniq_usages.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+module Omca
+  module Authorities
+    class UniqUsages
+      def self.desc = "Write one row per used refname, with count of "\
+        "usages. Note that different refnames for the same term record may "\
+        "have been used, so this output may still have multiple rows per term"
+
+      def self.call = new.call
+
+      def initialize
+        @srcpath = Omca::Authorities.usages_path
+        @outpath = Omca::Authorities.uniq_usages_path
+        @counter = {}
+      end
+
+      def call
+        populate_counter
+        write_csv
+      end
+
+      private
+
+      attr_reader :srcpath, :outpath, :counter
+
+      def populate_counter
+        File.open(srcpath) do |file|
+          CSV.foreach(file, headers: true) do |row|
+            refname = row["refname"]
+            counter[refname] = 0 unless counter.key?(refname)
+            counter[refname] += 1
+          end
+        end
+      end
+
+      def write_csv
+        CSV.open(
+          outpath,
+          "w",
+          headers: Omca::Authorities.uniq_usages_headers,
+          write_headers: true
+        ) do |csv|
+          counter.each do |refname, ct|
+            base = {
+              "usagect" => ct
+            }
+            termdata = Omca::Refname.add_parsed_detail(base, refname)
+            csv << termdata.values_at(*csv.headers)
+          end
+        end
+        puts "Wrote unique authority usages to #{outpath}"
+      end
+    end
+  end
+end
--- lib/tasks/at.thor
+++ lib/tasks/at.thor
@@ -7,36 +7,8 @@ class At < Thor
     Omca::Authorities::Usages.call
   end
 
-  desc "unique_usages", "Write one row per used refname, with count of "\
-    "usages. Note that different refnames for the same term record may have "\
-    "been used, so this output may still have multiple rows per term"
-  def unique_usages
-    srcpath = Omca::Authorities.usages_path
-    outpath = Omca::Authorities.uniq_usages_path
-    counter = {}
-
-    File.open(srcpath) do |file|
-      CSV.foreach(file, headers: true) do |row|
-        refname = row["refname"]
-        counter[refname] = 0 unless counter.key?(refname)
-        counter[refname] += 1
-      end
-    end
-
-    CSV.open(
-      outpath,
-      "w",
-      headers: Omca::Authorities.uniq_usages_headers,
-      write_headers: true
-    ) do |csv|
-      counter.each do |refname, ct|
-        base = {
-          "usagect" => ct
-        }
-        termdata = Omca::Refname.add_parsed_detail(base, refname)
-        csv << termdata.values_at(*csv.headers)
-      end
-    end
-    puts "Wrote unique authority usages to #{outpath}"
+  desc "uniq_usages", Omca::Authorities::UniqUsages.desc
+  def uniq_usages
+    Omca::Authorities::UniqUsages.call
   end
 end
