53baa7fd978937163c5390f23e40d15071e025bd
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-06-22 18:30:48 -0400

    refactor: Make big_auth file names consistent

--- lib/omca.rb
+++ lib/omca.rb
@@ -126,10 +126,10 @@ Kiba::Extend::ProjectConfig.config.graph_dir = File.join(
 Kiba::Extend.config.config_namespaces = [Omca]
 
 Omca::BigAuthFcar.config.provided_worksheets = [
-  "big_auth_worksheet0.csv"
+  "big_auth_worksheet_0.csv"
 ]
 Omca::BigAuthFcar.config.returned_files = [
-  "big_auth_worksheet0.csv"
+  "big_auth_worksheet_0.csv"
 ]
 
 Omca::NonRefnameAuthManualFcar.config.provided_worksheets = [
