e58430978a893740f48e376165e422be48be215f
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-07-27 20:38:34 -0400

    refactor: nuke_bom jobs to dynamic jobs

--- lib/omca/jobs/nuke_bom.rb
+++ lib/omca/jobs/nuke_bom.rb
@@ -3,30 +3,18 @@
 module Omca
   module Jobs
     class NukeBom
-      def self.desc = "Remove BOM from all orig data values"
+      include Omca::DynamicCsvJobable
 
-      def self.run(...) = new(...).run
+      attr_reader :source, :destination
 
-      def initialize(source:, dest:, table:, rectype:, tabletype:)
+      def initialize(source:, dest:, table: nil, rectype: nil, tabletype: nil)
         @source = source
         @destination = dest
-        @sourcepath = Omca.registry.resolve(source).path
-        @destpath = Omca.registry.resolve(destination).path
       end
 
-      def run
-        # FileUtils.cp(sourcepath, destpath)
-        `sed 's/\xef\xbb\xbf//g' #{sourcepath} > #{destpath}`
-        puts "Wrote to #{destpath}"
+      def job_code
+        `sed 's/\xef\xbb\xbf//g' #{source_path} > #{destination_path}`
       end
-
-      def outrows
-        `csvstat --count #{destpath}`
-      end
-
-      private
-
-      attr_reader :source, :destination, :sourcepath, :destpath
     end
   end
 end
