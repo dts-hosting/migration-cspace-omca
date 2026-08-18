4083af73b8b89e30ffe115299454a4e02973eec0
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-08-17 22:15:01 -0400

    refactor: Extract optlist downcase fields to config module
    @wsshaw - A benefit of making these Dry::Configurable config settings
    is that they are set on first call and then remain static. So you
    get memory/processing caching + immutability without throwing
    odd instance variables around everywhere

--- lib/omca/config/opt_list.rb
+++ lib/omca/config/opt_list.rb
@@ -24,5 +24,52 @@ module Omca
       end
       result
     end
+
+    # @return [Array<String>] List of source table/fields being mapped into
+    #   target fields controlled by option lists, so the values need to
+    #   be downcased
+    setting :downcase_field_list,
+      reader: true,
+      default: [
+        "acquisitions_common.acquisitionmethod",
+        "addressgroupomca.addresstypeomca",
+        "collectionobjects_common.recordstatus",
+        "concepttermgroup.historicalstatus",
+        "concepttermgroup.termtype",
+        "conditionchecks_common.conditioncheckreason",
+        "conditionchecks_omca_omcaconditioncheckmethods.item",
+        "hazardgroup.hazard",
+        "intakes_common.entryreason",
+        "loctermgroup.termstatus",
+        "loctermgroup.termtype",
+        "media_common_typelist.item",
+        "movements_common.currentlocationfitness",
+        "movements_common.reasonformove",
+        "orgtermgroup.termtype",
+        "persontermgroup.termtype",
+        "places_common.placetype",
+        "placetermgroup.historicalstatus",
+        "placetermgroup.termtype",
+        "restrictedmedia_common_typelist.item",
+        "taxon_common.taxonrank",
+        "taxontermgroup.termtype",
+        "titlegroup.titletype",
+        "valuationcontrols_common.valuetype"
+      ]
+
+    # @return [Hash] Fields that need to be downcased, grouped by table
+    #   for easy use in FixTableData transforms
+    setting :downcase_field_config,
+      reader: true,
+      default: {},
+      constructor: ->(_default) do
+        downcase_field_list.map do |val|
+          parts = val.split(".")
+          [parts[0], parts[1].to_sym]
+        end.group_by { |e| e[0] }
+          .transform_values do |v|
+            v.flatten.select { |val| val.is_a?(Symbol) }
+          end
+      end
   end
 end
