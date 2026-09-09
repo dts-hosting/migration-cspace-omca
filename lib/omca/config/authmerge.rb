ee515e9287a54087da74a7f9282cc319ea8ed513
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-09-09 15:27:36 -0400

    fix: Job run dependency in register files

--- lib/omca/config/authmerge.rb
+++ lib/omca/config/authmerge.rb
@@ -6,16 +6,18 @@ module Omca
 
     def usages_key = :authorities__usages_new_refname
 
-    def merge_ready? = Kiba::Extend::Job.output?(usages_key)
+    def usages_path = Omca.registry.resolve(usages_key)[:path]
 
-    def usages_path = Omca.registry
-      .resolve(usages_key)[:path]
+    def merge_prep_ready? = Omca.ready_for_authority_merge? &&
+      File.exist?(usages_path)
 
     def field_list = @field_list ||= get_field_list
 
     def by_table = @by_table ||= get_by_table
 
     def get_field_list
+      return [] unless merge_prep_ready?
+
       cmd = %(xan map 'col(1) ++ "." ++ col(3) as tablefield' ) +
         %(#{usages_path} | ) +
         %(xan select tablefield | ) +
@@ -24,8 +26,12 @@ module Omca
     end
     private_class_method :get_field_list
 
-    def get_by_table = field_list.group_by { |arr| arr[0] }
-      .transform_values { |v| v.map { |arr| arr[1] } }
+    def get_by_table
+      return {} unless merge_prep_ready?
+
+      field_list.group_by { |arr| arr[0] }
+        .transform_values { |v| v.map { |arr| arr[1] } }
+    end
     private_class_method :get_by_table
   end
 end
